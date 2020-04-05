0c8ec44039bca42c0751dd4f6ac804ad87b880a4
sidd_oo <sahoosiddharth3@gmail.com>
2020-04-06 01:26:12 +0530

    Credentials are fetched from cfile and website is getting hit in the chrome browser

--- activites/script.js
+++ activites/script.js
@@ -1,2 +1,23 @@
 let sd = require(`selenium-webdriver`);
 let cd = require('chromedriver');
+let fs = require('fs');
+let bldr = new sd.Builder();
+let driver = bldr.forBrowser('chrome').build();
+
+let cfile = process.argv[2];
+let mfile = process.argv[3];
+let cname = process.argv[4];
+
+let userName, pwd;
+
+let cfileWillBeReadPromise = fs.promises.readFile(cfile);
+cfileWillBeReadPromise.then(function (content) {
+    let credentials = JSON.parse(content);
+    userName = credentials.un;
+    pwd = credentials.pwd;
+
+    let loginPageWillBeLoadedPromise = driver.get(`https://www.pepcoding.com/login`);
+    return loginPageWillBeLoadedPromise;
+})
+
+
