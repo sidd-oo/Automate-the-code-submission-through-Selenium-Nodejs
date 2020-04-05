74b13e4984f19a0e5e2338a79d5e8ccc1dffac2c
sidd_oo <sahoosiddharth3@gmail.com>
2020-04-06 01:59:10 +0530

    Username and password will be entered Promise

--- activites/script.js
+++ activites/script.js
@@ -12,17 +12,26 @@ let userName, pwd;
 
 let cfileWillBeReadPromise = fs.promises.readFile(cfile);
 cfileWillBeReadPromise.then(function (content) {
+    // Parsing/Analysing the content of cfile
     let credentials = JSON.parse(content);
+    //Grabing the credentials i.e (username, password) by analysing the content of credential.json file 
     userName = credentials.un;
     pwd = credentials.pwd;
-
+    //This will allow us to navigate to the pepcoding's website and give a promise for further use.
     let loginPageWillBeLoadedPromise = driver.get(`https://www.pepcoding.com/login`);
     return loginPageWillBeLoadedPromise;
 }).then(function(){
+    //This will grab username and passwaord input elements and give a promise for further use.
     let unWillBeFoundPromise = driver.findElement(sd.By.css("input[type=email]"));
     let pwdWillBeFoundPromise = driver.findElement(sd.By.css("input[type=password]"));
     let bothElementsWillBeFoundPromise = Promise.all([unWillBeFoundPromise,pwdWillBeFoundPromise]);
     return bothElementsWillBeFoundPromise;
+}).then(function(elements){
+    //Username and password will be entered in the input fields of the login panel.
+    let userNameWillBeEnteredPromise = elements[0].sendKeys(userName);
+    let pwdWillBeEnteredPromise = elements[1].sendKeys(pwd);
+    let bothValueWillBeEnteredPromise = promises.all([userNameWillBeEnteredPromise,pwdWillBeEnteredPromise]);
+    return bothValueWillBeEnteredPromise;
 })
 
 
